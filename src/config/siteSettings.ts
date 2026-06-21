7d690e4fecb31b4b908a64c5a78c307482b4f3cd
Luka Šulc <lukasulc0@gmail.com>
2026-06-21 17:58:47 +0200

    Replace Google Analytics ID

--- src/config/siteSettings.ts
+++ src/config/siteSettings.ts
@@ -3,4 +3,4 @@ export type Locale = (typeof locales)[number];
 export const defaultLocale: Locale = "hr";
 export const localeMap: Record<Locale, string> = { hr: "hr-HR", en: "en-US" };
 export const languageSwitcherMap: Record<Locale, string> = { hr: "HR", en: "EN" };
-export const googleAnalyticsId = "G-ZDNRL6MNKB";
+export const googleAnalyticsId = "TODO - Add your Google Analytics ID here"; // TODO
